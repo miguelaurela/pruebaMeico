bedb46cc7850da33b2533d98df8bae1f16053fc6
Miguel Aurela <miguelaurela2804@gmail.com>
2023-05-05 02:11:29 -0500

    mostrar tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private httpClient: HttpClient){}
   title = 'pruebaMeico';
+  datos:any=[]
+
+  ngOnInit(): void {
+    this.llamarArchivo();
+  }
+  public llamarArchivo():void{
+    this.httpClient.get("assets/datos.json").subscribe(data =>{
+      this.datos=data;
+            
+    })
+  }
 }
